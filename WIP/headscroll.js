5cca366ac1c4400a05dbc805248e9eb1e6b9a7ff
TheCSimmons <TheCSimmons@Coltons-MacBook-Pro.local>
2016-09-15 15:36:52 -0600

    new nav

---
+++ WIP/headscroll.js
@@ -0,0 +1,44 @@
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('.header').addClass("scroll");
+  }
+  else{
+    $('.header').removeClass("scroll");
+  }
+});
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('.logocontainer').addClass("scroll");
+  }
+  else{
+    $('.logocontainer').removeClass("scroll");
+  }
+});
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('.logo').addClass("scroll");
+  }
+  else{
+    $('.logo').removeClass("scroll");
+  }
+});
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('.name').addClass("scroll");
+  }
+  else{
+    $('.name').removeClass("scroll");
+  }
+});
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
+    $('.navbar').addClass("scroll");
+  }
+  else{
+    $('.navbar').removeClass("scroll");
+  }
+});
